63f033aa70ebbd594774c358b308defc3f28e152
yangxvlin <1768528843@qq.com>
2020-10-23 15:22:53 +1100

    test hybrid

--- generate_slurm.py
+++ generate_slurm.py
@@ -72,11 +72,39 @@ def generate_slurm3(dierectory: str, nodes: list, file_format: str, datas: list)
             for nqueen, k in datas:
                 print("mpirun nqueen < ../nqueen_{}_{}.data > nqueen_{}_{}_{}.out".format(nqueen, k, n, nqueen, k), file=f)
 
+def generate_slurm4(dierectory: str, nodes: list, cores: list, file_format: str, bodies: list, minutes=15):
+    for n in nodes:
+        for core in cores:
+            with open("./" + dierectory + "/" + file_format.format(n, core) + ".slurm", "w") as f:
+                print("#!/bin/bash", file=f)
+                print("#SBATCH --time=0:{}:00".format(minutes), file=f)
+                print("# nodes=min-max", file=f)
+                print("#SBATCH --nodes={}".format(n), file=f)
+                print("#SBATCH --mem=32G", file=f)
+                print("#SBATCH --partition=snowy", file=f)
+                print("#SBATCH --ntasks-per-node=1", file=f)
+                print("#SBATCH --cpus-per-task={}".format(core), file=f)
+                print("#SBATCH --job-name=1-cpt-1-npn", file=f)
+                print("#SBATCH --output=script.out", file=f)
+                print("# You need to load a compiler before openmpi.", file=f)
+                print("", file=f)
+                print("module load gcc/8.3.0", file=f)
+                print("module load openmpi/3.1.4 ", file=f)
+                print("", file=f)
+                print("export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK", file=f)
+                print("", file=f)
+                print("mpicxx -std=c++14 -O3 -o {} {}.cpp".format(dierectory, dierectory), file=f)
+                for i in bodies:
+                    print("mpirun {} < ../body_{}.data > {}-1-{}.out".format(dierectory, i, n, i), file=f)
+
 if __name__ == "__main__":
     generate_slurm("n2_sequential", [1], "{}-1", [10, 100, 500, 1000, 2000], minutes=15)
     generate_slurm("n2_openmpi", [i for i in range(2, 13)], "{}-1", [10, 100, 500, 1000, 2000], minutes=15)
     generate_slurm("nlogn_sequential", [1], "{}-1", [10, 100, 500, 1000, 2000, 5000], minutes=15)
     generate_slurm("nlogn_openmpi", [i for i in range(2, 13)], "{}-1", [10, 100, 500, 1000, 2000, 5000], minutes=15)
+
+    generate_slurm4("nlogn_hybrid", [5], [i for i in range(2, 41)], "{}-{}", [500, 1000, 2000, 5000], minutes=30)
+    generate_slurm4("n2_hybrid", [5], [i for i in range(2, 41)], "{}-{}", [500, 1000, 2000, 5000], minutes=30)
     # datas = [(8, 90), (50, 90), (100, 90)]
     # generate_slurm2("mc_nqueen_parallel", [i for i in range(2, 13)], "{}", datas, 5)
     # generate_slurm3("mc_nqueen_parallel", [i for i in range(2, 13)], "{}", datas)
--- n2_hybrid/n2_hybrid.cpp
+++ n2_hybrid/n2_hybrid.cpp
@@ -65,10 +65,6 @@ inline void compute_force(int i, int N, double G, Body *n_bodies, Force * force)
     force->fz = 0;
 
     double px_diff, py_diff, pz_diff, factor, euclidean_distance;
-
-    // int n_threads = omp_get_max_threads();
-    // omp_set_num_threads(n_threads);
-    // #pragma omp prallel for
     for (int j = 0; j < N; j++) {
         if (i != j) {
             // distance in x direction
@@ -104,10 +100,10 @@ inline void update_body(Body * body_next, int N, double G, double TIME_DELTA, Bo
     body_next->vx += factor * body_force.fx;
     body_next->vy += factor * body_force.fy;
     body_next->vz += factor * body_force.fz;
+
 }
 
 inline void calculate(int N, int T, double G, double TIME_DELTA, Body *n_bodies) {
-    MPI_Bcast(&N, 1, MPI_INT, root, comm);
     MPI_Bcast(&T, 1, MPI_INT, root, comm);
     MPI_Bcast(&G, 1, MPI_DOUBLE, root, comm);
     MPI_Bcast(&TIME_DELTA, 1, MPI_DOUBLE, root, comm);
@@ -136,22 +132,14 @@ inline void calculate(int N, int T, double G, double TIME_DELTA, Body *n_bodies)
         }
     }
 
+    nt n_threads = omp_get_max_threads();
+    // cout << "rank[" << rank << "] " << n_threads << endl;
+    omp_set_num_threads(n_threads);
     // if (rank == root) {
     //     cout << "test 0 " << rank << endl;
     // }
 
     MPI_Bcast(n_nodies_padded, N, MPI_Body, root, comm);
-
-    int n_threads = omp_get_max_threads();
-    // // int n_per_thread = (int) ceil((1.0 * n_per_rank) / n_threads);
-    omp_set_num_threads(n_threads);
-    // #pragma omp parallel
-    // {
-    //     if (omp_get_thread_num() == 0) {
-    //         cout << "#threads used = " << omp_get_num_threads() << endl;
-    //     }
-    // }
-    // Force tmp_forces_buffer[n_threads][n_per_rank];
 
     for (int z = 0; z < T; ++z) {
         #pragma omp parallel for
@@ -159,23 +147,10 @@ inline void calculate(int N, int T, double G, double TIME_DELTA, Body *n_bodies)
             tmp_n_bodies[i - n_start] = n_nodies_padded[i];
         }
 
-        // #pragma omp parallel for schedule(static, n_per_thread)
         #pragma omp parallel for
         for (int i = n_start; i < n_end; ++i) {
-            int thread_rank = omp_get_thread_num();
             compute_force(i, N, G, n_nodies_padded, &(tmp_forces[i - n_start]));
-            // compute_force(i, N, G, n_nodies_padded, &(tmp_forces_buffer[thread_rank][i - n_start]));
         }
-        // int tmp = 0, cur_thread_rank = 0;
-        // for (int i = 0; i < n_end - n_start; ++i) {
-        //     tmp_forces[i] = tmp_forces_buffer[cur_thread_rank][i];
-        //     ++tmp;
-        //     if (tmp == n_per_thread) {
-        //         ++cur_thread_rank;
-        //         tmp = 0;
-        //     }
-        // }
-
         // if (rank == root) {
         //     cout << z << " force computed" << endl;
         // }
@@ -206,7 +181,6 @@ inline void calculate(int N, int T, double G, double TIME_DELTA, Body *n_bodies)
     }
 
     if (rank == root) {
-        #pragma omp parallel for
         for (int i = 0; i < N; ++i) {
             n_bodies[i] = n_nodies_padded[i];
         }
@@ -214,15 +188,6 @@ inline void calculate(int N, int T, double G, double TIME_DELTA, Body *n_bodies)
 }
 
 int main(int argc, char **argv) {
-    // int n_threads = omp_get_max_threads();
-    // omp_set_num_threads(n_threads);
-    // #pragma omp parallel
-    // {
-    //     if (omp_get_thread_num() == 0) {
-    //         cout << "#threads used = " << omp_get_num_threads() << endl;
-    //     }
-    // }
-
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(comm, &rank);
@@ -244,7 +209,7 @@ int main(int argc, char **argv) {
         cin >> G;
         cin >> TIME_DELTA;
     }
-
+    MPI_Bcast(&N, 1, MPI_INT, root, comm);
     Body n_bodies[N];
     if (rank == root) {
         for (int i = 0; i < N; ++i) {
@@ -274,5 +239,5 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-// mpicxx -std=c++14 -O3 -fopenmp -o n2_openmpi n2_openmpi.cpp
+// mpicxx -std=c++14 -O3 -o n2_openmpi n2_openmpi.cpp
 // mpirun -np 4 n2_openmpi < ../body_10.data
